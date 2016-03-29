21a334eacd20c6fbe3078b2f4ef49968e5ebff09
lino.larios <lino.larios@gmail.com>
2016-03-29 13:28:57 -0600

    project with gradle to build a microservice with springboot and docker

---
+++ Employee/src/main/java/com/lino/blog/EmployeeRepository.java
@@ -0,0 +1,8 @@
+package com.lino.blog;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface EmployeeRepository extends
+MongoRepository<Employee, String> {
+ 
+}
